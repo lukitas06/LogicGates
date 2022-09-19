fb3c764727591e672e57832fe52370742fd8e149
lukitas06 <lucasalegre13@gmail.com>
2022-09-19 18:53:15 -0300

    Initial commit

---
+++ src/Library/AND.cs
@@ -0,0 +1,15 @@
+﻿namespace logicGates
+{
+public class LogicAND
+{   
+    public string Name{get;}
+    public LogicAND(string name){
+        this.Name=name;
+    }
+    public void AgregarEntrada(string name, int value){
+        string[] entradas={} ;
+    }
+
+}
+
+}
